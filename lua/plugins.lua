eec8f9ddc7fe5a37fdf2d51689faad47d6045195
Nightmare9254 <adrian.sarna59@gmail.com>
2024-05-04 18:37:19 +0200

    feat: setup base kikstart nvim config

---
+++ lua/plugins.lua
@@ -0,0 +1,5 @@
+return {
+  --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
+  --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
+  { import = 'plugins' },
+}
