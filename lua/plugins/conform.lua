534d34ece2259650a32d09061972e3fce5c8e2c2
a.sarna <a.sarna@ideo.pl>
2024-07-03 09:29:34 +0200

    feat: update prettier, add wilder

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,26 +2,31 @@ return {
   'stevearc/conform.nvim',
   event = { 'BufReadPre', 'BufNewFile' },
   config = function()
-    local conform = require 'conform'
-
-    conform.setup {
+    require('conform').setup {
       formatters_by_ft = {
-        javascript = { 'prettier' },
-        typescript = { 'prettier' },
-        javascriptreact = { 'prettier' },
-        typescriptreact = { 'prettier' },
-        svelte = { 'prettier' },
-        css = { 'prettier' },
-        html = { 'prettier' },
-        json = { 'prettier' },
-        yaml = { 'prettier' },
-        markdown = { 'prettier' },
-        graphql = { 'prettier' },
-        liquid = { 'prettier' },
         lua = { 'stylua' },
-        python = { 'isort', 'black' },
+        svelte = { { 'prettierd', 'prettier' } },
+        javascript = { { 'prettierd', 'prettier' } },
+        typescript = { { 'prettierd', 'prettier' } },
+        javascriptreact = { { 'prettierd', 'prettier' } },
+        typescriptreact = { { 'prettierd', 'prettier' } },
+        json = { { 'prettierd', 'prettier' } },
+        graphql = { { 'prettierd', 'prettier' } },
+        java = { 'google-java-format' },
+        kotlin = { 'ktlint' },
+        ruby = { 'standardrb' },
+        markdown = { { 'prettierd', 'prettier' } },
+        erb = { 'htmlbeautifier' },
+        html = { 'htmlbeautifier' },
+        bash = { 'beautysh' },
+        proto = { 'buf' },
+        rust = { 'rustfmt' },
+        yaml = { 'yamlfix' },
+        toml = { 'taplo' },
+        css = { { 'prettierd', 'prettier' } },
+        scss = { { 'prettierd', 'prettier' } },
       },
-      format_on_save = function(bufnr)
+          format_on_save = function(bufnr)
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
@@ -35,12 +40,12 @@ return {
         }
       end,
     }
-
-    vim.keymap.set({ 'n', 'v' }, '<leader>mp', function()
+    local conform = require 'conform'
+    vim.keymap.set({ 'n', 'v' }, '<leader>l', function()
       conform.format {
         lsp_fallback = true,
         async = false,
-        timeout_ms = 1000,
+        timeout_ms = 500,
       }
     end, { desc = 'Format file or range (in visual mode)' })
   end,
--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,8 +1,23 @@
 return {
-  'lukas-reineke/indent-blankline.nvim',
-  event = { 'BufReadPre', 'BufNewFile' },
-  main = 'ibl',
-  opts = {
-    indent = { char = '┊' },
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    event = 'BufEnter',
+    opts = {
+      indent = {
+        char = '│',
+        tab_char = '│',
+      },
+      scope = { enabled = false },
+      exclude = {
+        filetypes = {
+          'help',
+          'lazy',
+          'mason',
+          'notify',
+          'oil',
+        },
+      },
+    },
+    main = 'ibl',
   },
 }
