38f8b25da47586917377baca74e5f292d8fc0f80
Nightmare9254 <adrian.sarna@tutanota.com>
2025-06-24 16:31:28 +0200

    feat: workspaces

--- hypr/.config/hypr/UserScripts/WorkspaceJob.sh
+++ hypr/.config/hypr/UserScripts/WorkspaceJob.sh
@@ -1,6 +1,9 @@
 hyprctl dispatch exec "[workspace 2 silent] firefox"
 hyprctl dispatch exec "[workspace 2 silent] brave"
 
+hyprctl dispatch exec "[workspace 6 silent] kitty"
+hyprctl dispatch exec "[workspace 6 silent] remmina"
+
 hyprctl dispatch exec "[workspace 4 silent] teams-for-linux"
 hyprctl dispatch exec "[workspace 4 silent] spotify-launcher"
 hyprctl dispatch exec "[workspace 4 silent] discord"
--- hypr/.config/hypr/UserScripts/WorkspaceVibe.sh
+++ hypr/.config/hypr/UserScripts/WorkspaceVibe.sh
@@ -2,7 +2,6 @@ hyprctl dispatch exec "[workspace 2 silent] firefox"
 
 hyprctl dispatch exec "[workspace 3 silent] kitty"
 hyprctl dispatch exec "[workspace 3 silent] kitty"
-hyprctl dispatch exec "[workspace 3 silent] docker"
 
 hyprctl dispatch exec "[workspace 4 silent] spotify-launcher"
 hyprctl dispatch exec "[workspace 4 silent] discord"
